c05aa6d2bb495f2edfb4501e362a893e13f4bc1f
Felix Ebert <felix.ebert@if-core.de>
2013-08-02 11:23:22 +0200

    service removed

--- pixelserver/src/main/java/de/ifcore/metis/admin/pixelserver/ServePixelController.java
+++ pixelserver/src/main/java/de/ifcore/metis/admin/pixelserver/ServePixelController.java
@@ -2,6 +2,8 @@ package de.ifcore.metis.admin.pixelserver;
 
 import javax.inject.Inject;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -11,18 +13,44 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @RequestMapping("/servePixel")
 public class ServePixelController
 {
-	private final ServePixelService service;
+	private static final String EMPTY_RESPONSE = "";
+	private static final Logger log = LoggerFactory.getLogger(ServePixelController.class);
+
+	private final PixelServer pixelServer;
 
 	@Inject
-	public ServePixelController(ServePixelService service)
+	public ServePixelController(PixelServer pixelServer)
 	{
-		this.service = service;
+		this.pixelServer = pixelServer;
 	}
 
 	@RequestMapping(method = RequestMethod.GET)
 	@ResponseBody
 	public String get(PageRequest pageRequest)
 	{
-		return service.getResponse(pageRequest);
+		try
+		{
+			String publicPixelId = pageRequest.toPage().getPublicPixelId(pixelServer);
+			return generateResponse(publicPixelId);
+		}
+		catch (Exception e)
+		{
+			log.error("couldn't determine publicPixelId for pageRequest", e);
+			return EMPTY_RESPONSE;
+		}
+	}
+
+	public String generateResponse(String publicPixelId)
+	{
+		if (publicPixelId == null)
+		{
+			return EMPTY_RESPONSE;
+		}
+		else
+		{
+			String src = "domain.met.vgwort.de/na/" + publicPixelId;
+			String imgTag = "<img src=\"" + src + "\" height=\"1\" width=\"1\" border=\"0\" />";
+			return "document.write('" + imgTag + "')";
+		}
 	}
 }
