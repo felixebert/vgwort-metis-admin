4371ba2f1d958f7b40ae2f45a759ba1d78e39518
Felix Ebert <felix.ebert@if-core.de>
2013-08-03 14:23:38 +0200

    pixelAdmin wip

---
+++ admin/src/main/java/de/ifcore/metis/admin/pixel/PixelServerImpl.java
@@ -0,0 +1,25 @@
+package de.ifcore.metis.admin.pixel;
+
+import org.springframework.stereotype.Service;
+
+import de.ifcore.metis.admin.pixelserver.PixelServer;
+
+@Service
+public class PixelServerImpl implements PixelServer
+{
+
+	@Override
+	public String getPublicPixelId(String pageId)
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String assignPixel(String pageId, String url)
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
