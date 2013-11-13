5e618f2b90c628e2f3d4c66be42b076a26194dc4
Felix Ebert <felix.ebert@if-core.de>
2013-11-13 22:44:11 +0100

    webpage: getId

--- grabber/src/main/java/de/ifcore/metis/grabber/WebPage.java
+++ grabber/src/main/java/de/ifcore/metis/grabber/WebPage.java
@@ -1,6 +1,9 @@
 package de.ifcore.metis.grabber;
 
+import java.util.List;
+
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 
 import de.ifcore.metis.grabber.GrabbedData.Builder;
 
@@ -24,6 +27,55 @@ public class WebPage
 	{
 		Builder builder = new GrabbedData.Builder(url);
 		builder.setResult(GrabResult.FOUND);
+		builder.setId(getId());
+		builder.setLyric(getLyric());
+		builder.setPageUrls(getPageUrls());
+		builder.setPdfUrl(getPdfUrl());
+		builder.setText(getText());
+		builder.setTitle(getText());
+		builder.setTranslators(getTranslators());
+		builder.setAuthors(getAuthors());
+		return builder.build();
+	}
+
+	public String getText()
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String getPdfUrl()
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public List<String> getPageUrls()
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Boolean getLyric()
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String getId()
+	{
+		Element metaTag = document.select("meta[name=vgw-id]").first();
+		return metaTag == null ? null : metaTag.attr("content");
+	}
+
+	public List<String> getTranslators()
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public List<String> getAuthors()
+	{
 		return null;
 	}
 }
--- grabber/src/test/java/de/ifcore/metis/grabber/WebPageTest.java
+++ grabber/src/test/java/de/ifcore/metis/grabber/WebPageTest.java
@@ -1,12 +1,42 @@
 package de.ifcore.metis.grabber;
 
+import static org.junit.Assert.assertEquals;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
 import org.junit.Test;
 
 public class WebPageTest
 {
 	@Test
-	public void testGetId()
+	public void testGetId() throws IOException
 	{
+		WebPage webPage = webPageFor("gta.html");
+		assertEquals("cheat-1234", webPage.getId());
+	}
 
+	private WebPage webPageFor(String filename) throws IOException
+	{
+		File file = getResource(filename);
+		Document document = Jsoup.parse(file, "UTF-8", "");
+		return new WebPage(null, document);
+	}
+
+	private File getResource(String filename)
+	{
+		URL url = getClass().getResource("/" + filename);
+		try
+		{
+			return new File(url.toURI());
+		}
+		catch (URISyntaxException e)
+		{
+			throw new IllegalStateException(e);
+		}
 	}
 }
